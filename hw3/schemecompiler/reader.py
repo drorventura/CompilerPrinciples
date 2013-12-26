c99fe71351e82a2f5ef4157186363d6afc182a31
Eldar Damari <eldar.damari@gmail.com>
2013-12-26 17:27:46 +0200

    updating files from previous assignment.

--- hw3/schemecompiler/reader.py
+++ hw3/schemecompiler/reader.py
@@ -18,6 +18,9 @@ commentParser = ps  .parser(pc.pcChar(';')) \
                     .parser(pc.pcWord("\\n")) \
                     .caten().pack(lambda x: '') \
                     .done()
+                    
+                    #.star() \
+                    #.caten().pack(lambda x: '') \
 
 # Parser for the Boolean sexpression
 boolParser = ps .const(lambda x: x == '#') \
@@ -282,9 +285,6 @@ quoteLikeFormsParser = ps   .parser(pc.pcChar("'")).pack(lambda x: sexprs.Symbol
 
 # Sexpression parser - Main parsing function
 sexpression = ps   .parser(commentParser) \
-                   .parser(pc.pcWhite1) \
-                   .disj() \
-                   .star() \
                    .maybe() \
                    .parser(fractionParser) \
                    .parser(hexNumberParser) \
