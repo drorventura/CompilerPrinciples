6d06ce22ee458a503e0e856bab57220eae2163f7
drorventura <ventura2005@gmail.com>
2014-01-11 17:04:17 +0200

    the file compiler.py

---
+++ hw4/schemecompiler/compiler.py
@@ -0,0 +1,19 @@
+__author__ = 'Dror Ventura & Eldar Damari'
+
+memoryTable = { 'void':[1,'T_VOID'],'nil':[2,'T_NIL'] }
+symbolTable = {'+':'T_PLUS' , '-':'T-MINUS' , '*':'T-MULTI' , '/':'T-DIVIDE'}
+
+mem0 = 7
+
+@staticmethod
+def compile_scheme_file(source, target):
+    print ('insert code for compile here')
+
+    # result += 'PUSH(' + integer + ');\n'
+    # result += 'CALL(MAKE_SOB_INTEGER);\n'
+    # result += 'DROP(1);\n'
+    # result += 'PUSH R1;\n'
+    # result += 'MOV(R1,MEM(0));\n'
+    # result += 'MOV(IND(R1),R0);\n'
+    # result += 'DROP(1);\n'
+    # result += 'ADD(MEM(0),IMM(2));\n'
