4b88d18859b520b9c2513110ddce68365862a480
drorventura <ventura2005@gmail.com>
2014-02-10 21:48:55 +0200

    is_sob_nil.asm now supports our compiler

--- hw4/arch/lib/scheme/is_sob_nil.asm
+++ hw4/arch/lib/scheme/is_sob_nil.asm
@@ -3,19 +3,19 @@
  * (long, not Scheme integer objects or Scheme boolean objets),
  * depending on whether the argument is nil.
  * 
- * Programmer: Mayer Goldberg, 2010
+ * Programmer: Dror ventura, 2014
  */
 
  IS_SOB_NIL:
   PUSH(FP);
   MOV(FP, SP);
-  MOV(R0, FPARG(0));
+  MOV(R0, FPARG(2));
   CMP(IND(R0), T_NIL);
   JUMP_EQ(L_IS_SOB_NIL_TRUE);
-  MOV(R0, IMM(0));
+  MOV(R0, IMM(3));
   JUMP(L_IS_SOB_NIL_EXIT);
  L_IS_SOB_NIL_TRUE:
-  MOV(R0, IMM(1));
+  MOV(R0, IMM(5));
  L_IS_SOB_NIL_EXIT:
   POP(FP);
   RETURN;
