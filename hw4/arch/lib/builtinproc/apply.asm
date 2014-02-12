aecac9703b99a7badcbe6f6483ead5877fd4f55e
Eldar Damari <eldar.damari@gmail.com>
2014-02-12 19:04:23 +0200

    lib/builtinproc/ :: apply.asm -> working fine

--- hw4/arch/lib/builtinproc/apply.asm
+++ hw4/arch/lib/builtinproc/apply.asm
@@ -1,5 +1,5 @@
 
-L_APPLY:
+L_APPLY_APPLIC:
 
 	PUSH(FP);
 	MOV(FP,SP);
@@ -7,72 +7,51 @@ L_APPLY:
 	PUSH(R1);
 	PUSH(R2);
 	PUSH(R3);
-	PUSH(R5);
-	PUSH(R6);	
-		
-	MOV(R1,FPARG(2)); 	/* closure */
-	MOV(R2,FPARG(3)); 	/* params */
 	
-	MOV(R5,FPARG(2)); 	/* closure */
-	MOV(R0,11);      	/* will hold reverse list */
-	MOV(R6,0);
-	
-	CMP(R2,11);
-	JUMP_EQ(APPLY_REVERSE_END2);
-	
-APPLY_REVERSE:
-	CMP(R2,11);
-	JUMP_EQ(APPLY_REVERSE_END);
-	PUSH(R0);
-	PUSH(INDD(R2,1));
-	CALL(MAKE_SOB_PAIR);
-	DROP(2);
-	MOV(R2,INDD(R2,2));
-	INCR(R6);
-	JUMP(APPLY_REVERSE);
-	
-APPLY_REVERSE_END:
-	
-APPLY_PUSH:
-	CMP(R0,11);
-	JUMP_EQ(APPLY_PUSH_END);
-	PUSH(INDD(R0,1));
-	MOV(R0,INDD(R0,2));
-	JUMP(APPLY_PUSH);
-	
-APPLY_PUSH_END:
-	MOV(R3,SP);
-	SUB(R3,FP);
-	PUSH(R6);			/* num of params */
-	PUSH(INDD(R5,1));	/* env */
-	CALLA(INDD(R5,2));
-	DROP(2);
-	DROP(IMM(R6));
-	
-	POP(R6);
-	POP(R5);
-	POP(R3);
-	POP(R2);
-	POP(R1);	
-	
-	MOV(SP,FP);
-	POP(FP);
-	RETURN;
-	
-APPLY_REVERSE_END2:
-	PUSH(11);			/* nil*/
-	PUSH(1);			/* num of args*/
-	PUSH(INDD(R5,1));	/* env */
-	
-	CALLA(INDD(R5,2));
-	DROP(3);
-	
-	POP(R6);
-	POP(R5);
-	POP(R3);
-	POP(R2);
-	POP(R1);	
-	
-	MOV(SP,FP);
-	POP(FP);
-	RETURN;
+    MOV(R0,FPARG(3));
+
+L_APPLY_PARAMS_LOOP:
+    CMP(R0,IMM(2));
+    JUMP_EQ(L_APPLY_END_PARAMS);
+
+    PUSH(INDD(R0,IMM(1)));
+    MOV(R0,INDD(R0,IMM(2)));
+    JUMP(L_APPLY_PARAMS_LOOP);
+
+L_APPLY_END_PARAMS:
+    
+    MOV(R1,(IMM(-1)));
+    MOV(R2,SP);
+    SUB(R2,FP);
+    DECR(R2);
+
+L_APPLY_REVERSE_LOOP:
+    CMP(R1,R2);
+    JUMP_LT(L_APPLY_END_REVERSE)
+    JUMP_EQ(L_APPLY_END_REVERSE)
+    MOV(R3,STARG(R1));
+    MOV(STARG(R1),STARG(R2));
+    MOV(STARG(R2),R3);
+    DECR(R1);
+    INCR(R2);
+    JUMP(L_APPLY_REVERSE_LOOP);
+
+L_APPLY_END_REVERSE:
+
+    MOV(R1,SP);
+    SUB(R1,FP);
+    SUB(R1,IMM(3));
+    PUSH(R1);
+    MOV(R2,FPARG(IMM(2)));
+    PUSH(INDD(R2,IMM(1)));
+    CALLA(INDD(R2,IMM(2)));
+
+    DROP(IMM(2));
+    DROP(R1);
+
+    POP(R3);
+    POP(R2);
+    POP(R1);
+    POP(FP);
+
+    RETURN;
